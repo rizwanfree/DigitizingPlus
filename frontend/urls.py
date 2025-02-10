e8f8e1a56e225115acc9852c4d4eb3a329505d03
rizwanfree <rizwanfreelance2022@gmail.com>
2025-02-11 04:32:11 +0500

    web pages

--- frontend/urls.py
+++ frontend/urls.py
@@ -2,6 +2,15 @@ from django.urls import path
 
 from . import views
 
+
+app_name = 'web'
+
 urlpatterns = [
     path('', views.index, name='index'),
+    path('features/', views.features, name='features'),
+    path('services/', views.services, name='services'),
+    path('portfolio/', views.portfolio, name='portfolio'),
+    path('pricing/', views.pricing, name='pricing'),
+    path('payment/', views.payment, name='payment'),
+    path('contact/', views.contact, name='contact'),
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -5,3 +5,21 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "frontend/index.html")
+
+def features(request):
+    return render(request, "frontend/features.html")
+
+def services(request):
+    return render(request, "frontend/services.html")
+
+def portfolio(request):
+    return render(request, "frontend/portfolio.html")
+
+def pricing(request):
+    return render(request, "frontend/pricing.html")
+
+def payment(request):
+    return render(request, "frontend/payment.html")
+
+def contact(request):
+    return render(request, "frontend/contact.html")
