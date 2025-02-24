ae991d41e1ba84116a7f07371ed544c1a4eb22e9
rizwanfree <rizwanfreelance2022@gmail.com>
2025-02-24 17:34:37 +0500

    allowed all hosts

--- digitizingplus/settings.py
+++ digitizingplus/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-yq@k!mpgp0@6zhr7g3wp*+60ej-88mkb()ful2_^89$1&qv!(5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
