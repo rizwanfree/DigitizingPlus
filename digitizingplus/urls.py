f72a364f67eb69c5fd7465def1f88d9df3435893
rizwanfree <rizwanfreelance2022@gmail.com>
2025-07-03 03:38:29 +0500

    updated admin url

--- digitizingplus/urls.py
+++ digitizingplus/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('backoffice/dev/admin/', admin.site.urls),
     path('', include('frontend.urls')),
     path('user/', include('users.urls')),
     path('craft/', include('crafting.urls')),
